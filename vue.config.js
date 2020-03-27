7f163ce1733d9fa8a67e8df060b9d50fc65296d4
skru <skruproxy@gmail.com>
2020-03-27 10:18:09 +0000

    service worker .htaccess caching fixed, basic PWA functionality

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,22 @@
 module.exports = {
     pwa: {
         workboxOptions: {
-            skipWaiting: true
+            skipWaiting: true,
+//             exclude: [
+
+//               //"assets/.htaccess"
+//             ]
+        },
+        themeColor: "#FFFFFF",
+        msTileColor: '#000000',
+    	appleMobileWebAppCapable: 'yes',
+    	appleMobileWebAppStatusBarStyle: 'white',
+        manifestOptions: {
+            name: "ProactiveHealthCare",
+        	short_name: "PHC",
+        	start_url: "https://jm1179.xyz/",
+        	display: "standalone",
+            theme_color: "#FFFFFF"
         }
     }
 }
